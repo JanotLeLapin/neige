12fceff6a9f7f6ba11b0e872f572b46bcc6c791b
Joseph DALY <joseph300905@gmail.com>
2024-01-09 22:52:26 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    overlay.url = "github:oxalica/rust-overlay";
+    crate2nix.url = "github:nix-community/crate2nix";
+  };
+
+  outputs = {
+    nixpkgs,
+    overlay,
+    crate2nix,
+    ...
+  }: let
+    eachSystem = fn: nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+    ] (system: (fn {
+      inherit system;
+      pkgs = (import nixpkgs { inherit system; overlays = [ (import overlay) ]; } );
+    }));
+  in {
+    devShells = eachSystem ({ pkgs, ... }: {
+      default = pkgs.mkShell { packages = [ (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml) ]; };
+    });
+    packages = eachSystem ({ pkgs, system }: let
+      cargoNix = crate2nix.tools.${system}.appliedCargoNix {
+        name = "rust";
+        src = ./.;
+      };
+    in {
+      default = cargoNix.workspaceMembers.app.build;
+    });
+  };
+}
