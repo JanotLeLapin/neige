f91af14d655ad4441d53c7cc6f8716c3cc414aca
Joseph DALY <joseph300905@gmail.com>
2024-01-10 15:08:35 +0100

    keyring

--- app/src/main.rs
+++ app/src/main.rs
@@ -4,12 +4,21 @@ use tokio::sync::mpsc;
 
 #[tokio::main]
 pub async fn main() {
+    let entry = keyring::Entry::new("neige", &whoami::username()).unwrap();
+    let token = match std::env::args().skip(1).next() {
+        Some(token) => {
+            let _ = entry.set_password(&token);
+            token
+        },
+        None => { entry.get_password().expect("could not get password from keyring") },
+    };
+
     let (mut tx, mut rx) = mpsc::channel(32);
 
-    tokio::spawn(async move { rest::rest_task(&mut rx).await });
+    tokio::spawn(async move { rest::rest_task(&token, &mut rx).await });
 
-    let a = rest::send(&mut tx, "/users/@me", None).await.unwrap();
-    let b = rest::send(&mut tx, "/users/foo", None).await.unwrap();
-
-    println!("{a}, {b}");
+    let user = rest::wrapper::get_current_user(&mut tx).await;
+    let other = rest::wrapper::get_user(&mut tx, "957286680795156510").await;
+    println!("{user}");
+    println!("{other:?}");
 }
